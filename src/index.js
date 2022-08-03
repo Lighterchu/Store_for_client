8182ab221075c4d3c71965f8a089c4b491252b4e
Benjamin <benjaminrowe0@gmail.com>
2022-08-04 02:16:44 +1000

    fixing errors

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import Client from 'shopify-buy';
-import { QueryClient, QueryClientProvider } from 'react-query';
+// import { QueryClient, QueryClientProvider } from 'react-query';
 
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <QueryClientProvider client={queryClient}>
+  //<QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientProvider>
+  //</QueryClientProvider>
 );
